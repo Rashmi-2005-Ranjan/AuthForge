87b35f5c1c5464910c8356549298dbf86ab585b2
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-09-01 22:42:22 +0530

    Frontend Project

---
+++ authforge-backend/src/test/java/in/authforge/AuthforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.authforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthforgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
