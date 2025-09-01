7c683b213b2799030131b28a7bf36452e95bd16d
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-09-01 22:42:22 +0530

    Maven Project Created

---
+++ authforge-backend/src/main/java/in/authforge/AuthforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package in.authforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthforgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthforgeBackendApplication.class, args);
+	}
+
+}
