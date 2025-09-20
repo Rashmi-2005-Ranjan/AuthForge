eb14a0007c2d4909d2d94e73c5ccdae14f0f5dac
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-09-21 03:03:58 +0530

    React Router and Routes are Added and app constant.js file

--- authforge-frontend/src/App.jsx
+++ authforge-frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import {ToastContainer} from "react-toastify";
+import {Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import Login from "./Pages/Login.jsx";
+import EmailVerify from "./Pages/EmailVerify.jsx";
+import ResetPassword from "./Pages/ResetPassword.jsx";
+
 const App = () => {
     return (
         <div>
-            <h1>Hello Welcome To AuthForge</h1>
+            <ToastContainer/>
+            <Routes>
+                {/* Define your routes here */}
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/email-verify" element={<EmailVerify/>}/>
+                <Route path="/reset-password" element={<ResetPassword/>}/>
+            </Routes>
         </div>
     )
 }
